3e15e0c4ea12025042fd96a02065669f81949361
Pavlina Bortlova <pbortlov@redhat.com>
2021-03-25 14:17:15 +0100

    v0.18.0 Changelog

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ if os.environ.get("READTHEDOCS", None):
 
 setup(
     name="pubtools-iib",
-    version="0.17.0",
+    version="0.18.0",
     description="Pubtools-iib",
     long_description=long_description,
     author="Jindrich Luza",
