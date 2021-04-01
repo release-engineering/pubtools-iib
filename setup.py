1ae2db84ae9ffccb10213ff7469edd41d662b190
Lubomir Gallovic <lgallovi@redhat.com>
2021-04-01 12:26:56 +0200

    v0.19.0 Changelog

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ if os.environ.get("READTHEDOCS", None):
 
 setup(
     name="pubtools-iib",
-    version="0.18.1",
+    version="0.19.0",
     description="Pubtools-iib",
     long_description=long_description,
     author="Jindrich Luza",
