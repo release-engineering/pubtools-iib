b76da00323062b60e14323b4491649ca7dfd9fe2
xdaile <mychalze@gmail.com>
2023-09-12 18:16:21 +0200

    Release 0.23.0

--- setup.py
+++ setup.py
@@ -64,7 +64,7 @@ if os.environ.get("READTHEDOCS", None):
 
 setup(
     name="pubtools-iib",
-    version="0.22.0",
+    version="0.23.0",
     description="Pubtools-iib",
     long_description=long_description,
     author="Jindrich Luza",
