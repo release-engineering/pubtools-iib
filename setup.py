8be0b0281b5accbdd5917b0665c0daecd7a3270e
Aidan McMahon-Smith <amcmahon@redhat.com>
2024-05-21 13:37:14 +0200

    Use pubtools namespace to resolve init file conflict [RHELDST-17607]
    Currently, there's an issue with packaging where most of the pubtool-<lib> projects
    contains __init__.py and other stuff which is supposed to belong only to pubtools.
    Python namespaces are a convenient way to work around this issue.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ import os
 
 # import pkg_resources
 import sys
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 from setuptools.command.test import test as TestCommand
 
 
@@ -72,7 +72,8 @@ setup(
     url="https://gitlab.cee.redhat.com/jluza/pubtools-iib",
     classifiers=classifiers,
     python_requires=">=3.6",
-    packages=find_packages(exclude=["tests"]),
+    packages=find_namespace_packages(where="src"),
+    package_dir={"": "src"},
     data_files=[],
     install_requires=INSTALL_REQURIES,
     dependency_links=DEPENDENCY_LINKS,
